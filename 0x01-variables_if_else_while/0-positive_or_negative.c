d08654458b9812c73dcfc79da80ccac9fb634298
Nomcebo <nmcebo23@gmail.com>
2022-12-09 19:09:02 -0800

    Postive & negative var

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 
 /*
  * main - Entry point
- * Description: Print positive or negative according to if
+ * Description: prints if a number is positive, negative or zero.
  * Return: Always 0 (Success)
  */
 
@@ -15,10 +15,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
+	{
 		printf("%d is positive\n", n);
+	}
 	else if (n == 0)
+	{
 		printf("%d is zero\n", n);
+	}
 	else
+	{
 		printf("%d is negative\n", n);
+	}
 	return (0);
 }
