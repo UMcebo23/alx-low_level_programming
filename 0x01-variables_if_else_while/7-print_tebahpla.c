087a103f4e7a6deff4e0a51c9e4301d472f6dbff
Nomcebo <nmcebo23@gmail.com>
2022-12-15 05:20:37 -0800

    Print letters reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints the lowercase alphabet in reverse
+ * Return: Always 0 (Succes)
+ */
+
+int main(void)
+{
+	char a;
+
+	or (a = 'z' ; a >= 'a' ; a--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
