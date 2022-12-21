dc78c93cb35167c4a4de2236b12ca62d9e0c2df7
Nomcebo <nmcebo23@gmail.com>
2022-12-21 07:42:20 -0800

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Description: Print putchar
+ * Return: 0 Always
+ */
+
+int main(void)
+{
+	char *p = "_putchar\n";
+
+	while (*p)
+	{
+		_putchar(*p);
+		p++;
+	}
+	_putchar('\n');
+	return (0);
+}
