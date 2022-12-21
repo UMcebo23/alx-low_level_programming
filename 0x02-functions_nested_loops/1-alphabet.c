8ee2681a8eaad4809e357753692e34c5db8ed1fb
Nomcebo <nmcebo23@gmail.com>
2022-12-21 08:49:06 -0800

    Print lowercase alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: Print alphabets lowercase
+ * Return: 0 Always
+ */
+
+void print_alphabet(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; aplh++)
+	{
+		_putchar(alph);
+	}
+	_putchar('\n');
+	Return (0);
+}
