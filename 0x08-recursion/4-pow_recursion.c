c903673bf3c07543f4d9e460ac9cc4bc8cedca67
Nomcebo <nmcebo23@gmail.com>
2023-01-06 06:41:17 -0800

    Return to the power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - function that returns the value of x to the power y
+ * @x: value to be manipulated
+ * @y: manipulator
+ * Return: -1 if y < 0
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (!y)
+		return (1);
+	return (x * _pow_recursion(x, --y));
+}
