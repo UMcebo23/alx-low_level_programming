0fa0c235399699520f3b63052230f699a2a448a5
Nomcebo <nmcebo23@gmail.com>
2023-01-06 04:11:24 -0800

    Print String, new line

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,11 +9,12 @@
 
 void _puts_recursion(char *s)
 {
-	if (!*s)
+	if (*s)
 	{
-		_putchar('\n');
-		return;
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	putchar(*s);
-	_puts_recursion((s + 1));
+
+	else
+		_putchar('\n');
 }
